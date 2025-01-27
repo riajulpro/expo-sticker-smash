754d56d8b483a8ea1777fcbc44197b770b924717
Riajul Islam <riajulpro@outlook.com>
2025-01-27 11:34:21 +0600

    Adding expo-image and expo-image-picker

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,49 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import { useState } from "react";
+
+const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Expo Man.</Text>
-      <Link href="/about" style={styles.text}>
-        About
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -20,7 +56,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     height: "100%",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
